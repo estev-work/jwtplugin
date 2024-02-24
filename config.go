0b55c844b40a5cf0e3046359729f60995d70fb8b
Alexei Gybchenko <alexey.g@pyrobyte.ru>
2024-02-24 21:51:29 +0700

    fix config, rename plugin

--- config.go
+++ config.go
@@ -1,6 +1,12 @@
-package jwtplugin
+package jwt
 
 // Config содержит конфигурационные параметры для плагина.
 type Config struct {
 	Secret string `mapstructure:"secret"`
+}
+
+func (cfg *Config) InitDefaults() {
+	if cfg.Secret == "" {
+		cfg.Secret = "secret"
+	}
 }
--- plugin.go
+++ plugin.go
@@ -1,9 +1,10 @@
-package jwtplugin
+package jwt
 
 import (
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/roadrunner-server/config/v4"
+	"github.com/roadrunner-server/errors"
 	"github.com/roadrunner-server/logger/v4"
+	"go.uber.org/zap"
 	"net/http"
 	"strings"
 )
@@ -17,18 +18,34 @@ type Plugin struct {
 	logger logger.Logger
 }
 
-func (p *Plugin) Init(cfg config.Plugin, logger logger.Logger) error {
-	var appendConfig Config
-	if err := cfg.UnmarshalKey("jwt_plugin", &appendConfig); err != nil {
-		return err
+type Configurer interface {
+	// UnmarshalKey takes a single key and unmarshal it into a Struct.
+	UnmarshalKey(name string, out any) error
+	// Has checks if config section exists.
+	Has(name string) bool
+}
+
+type Logger interface {
+	NamedLogger(name string) *zap.Logger
+}
+
+func (s *Plugin) Init(cfg Configurer) error {
+	const op = errors.Op("jwt_plugin_init")
+	if !cfg.Has(PluginName) {
+		return errors.E(op, errors.Disabled)
 	}
 
-	p.cfg = &appendConfig
-	p.logger = logger
+	err := cfg.UnmarshalKey(PluginName, &s.cfg)
+	if err != nil {
+		return errors.E(op, err)
+	}
+
+	s.cfg.InitDefaults()
+	s.logger.NamedLogger("jwt_logger")
 	return nil
 }
 
-func (p *Plugin) Middleware(next http.Handler) http.Handler {
+func (s *Plugin) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if !strings.HasPrefix(authHeader, "Bearer ") {
@@ -38,11 +55,11 @@ func (p *Plugin) Middleware(next http.Handler) http.Handler {
 
 		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
 		_, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-			return []byte(p.cfg.Secret), nil
+			return []byte(s.cfg.Secret), nil
 		})
 
 		if err != nil {
-			p.logger.NamedLogger("jwtplugin").Error("invalid JWT token")
+			s.logger.NamedLogger("jwtplugin").Error("invalid JWT token")
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
@@ -51,6 +68,6 @@ func (p *Plugin) Middleware(next http.Handler) http.Handler {
 	})
 }
 
-func (p *Plugin) Name() string {
+func (s *Plugin) Name() string {
 	return PluginName
 }
